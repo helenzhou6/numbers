b3370e18af36bc27e864968fe3c8c71fabd08bd9
Helen Zhou <helen.zhou6@gmail.com>
2017-06-27 19:55:49 +0100

    More UI improvements (e.g. was unable to delete 0)

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ $(document).ready(function () {
 
 	// updates the description: removes bolded property, and updates property and description
 	var descripUpdate = function(value, data) {
+
 		$('.property').find('.is-bold').removeClass('is-bold');
 		var effectiveValue = parseInt(value);
 		var description = "If you know a distinctive fact about this number, please <a href='mailto:efriedma@stetson.edu'>e-mail</a> me.";
@@ -22,44 +23,59 @@ $(document).ready(function () {
 				description = 'Type a number between <span class="number">0</span> to <span class="number">9999</span>';
 		}
 		$text.html(description);
-		// with fade animations:
-		// $text.fadeOut(50, function() {
-		// 	$(this).html(description).fadeIn(50);
-		// });
-	}
 
-	// INIT
-
-	// if there is already an input on refresh, updates the shadow
-	if ($input.val()){
-		shadow($input.val());
 	}
 
 	// UI
-	// on keydown, prevents more than 4 digits being typed
+	// on keydown, prevents more than 4 digits being typed and other characters
 	$input.on('keydown', function(e) {
-		if ($(this).val().length > 3
+		var allowedKeys = [46, 8, 13, 37, 39, 38, 40, 45, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105];
+			if (window.location.pathname.substring(1) === '0'
+			&& e.keyCode === 8
+			) {
+				$input.val('');
+				shadow('');
+				$('.property').find('.is-bold').removeClass('is-bold');
+				$text.html('Type a number between <span class="number">0</span> to <span class="number">9999</span>');
+				history.pushState({}, "", '/');
+			} else if ($(this).val().length > 3
 			&& e.keyCode != 46
 			&& e.keyCode != 8
 			&& e.keyCode != 37
 			&& e.keyCode != 39
-			|| e.keyCode === 69
-			|| e.keyCode === 189
-			|| e.keyCode === 190
-			|| e.keyCode === 187) {
-				e.preventDefault();
-			}
-    });
-
+			|| (allowedKeys.indexOf(e.keyCode) == -1)
+			|| window.location.pathname.substring(1) === '0'
+			&& e.keyCode === 40) {
+							e.preventDefault();
+				}
+		});
 	// once ajax done...
   $.ajax('/Numnumbers.json').done(function(data){
+
+
 		// focuses on input when document loaded (init)
 		$input.focus();
+
+		var urlValue = parseInt(window.location.pathname.replace(/\D/g, '').substr(0, 4));
+		if (urlValue || urlValue === 0){
+			$input.val(urlValue);
+			shadow(urlValue);
+			descripUpdate(urlValue, data);
+			history.pushState(urlValue, null, urlValue);
+		} else {
+			history.pushState({}, "", '/');
+		}
 
 		// on input in input field, updates shadow and description/property, adds to history/url
     $input.on('input', function(e) {
 			e.preventDefault();
-      var inputVal = $(this).val();
+			// this removes the being able to down arrow to negative numbers, but between 0 and 1
+			// also evaluates 000 to 0 and 099 to 99 in UI
+			var inputVal = '';
+			if ($(this).val() != ''){
+				var inputVal = Math.abs($(this).val());
+			}
+			$(this).val(inputVal);
 			shadow(inputVal);
 			descripUpdate(inputVal, data);
 			history.pushState(inputVal, null, inputVal);
